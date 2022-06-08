5d26cb915a7ee80121bd31714cda321f30d64458
Emilian Filip <emilian.filip20@imperial.ac.uk>
2022-06-08 17:22:26 +0100

    [ef420] [project] Added auto-generated Spring Boot files, including Gradle.

---
+++ src/main/kotlin/ac/ic/drp19/bookappbackend/BookappBackendApplication.kt
@@ -0,0 +1,11 @@
+package ac.ic.drp19.bookappbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookappBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookappBackendApplication>(*args)
+}
---
+++ src/test/kotlin/ac/ic/drp19/bookappbackend/BookappBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package ac.ic.drp19.bookappbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookappBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
