5bdf6c724252b6e560ebe9ad41f330492cfc51f8
Emilian Filip <emilian.filip20@imperial.ac.uk>
2022-06-09 23:46:43 +0100

    [ef420] [fix] Attempt to fix Heroku deployment

--- src/main/kotlin/ac/ic/drp19/bookappbackend/BookappBackendApplication.kt
+++ src/main/kotlin/ac/ic/drp19/bookappbackend/BookappBackendApplication.kt
@@ -1,21 +1,26 @@
-package ac.ic.drp19.bookappbackend
+package ac.ic.drp19.bookappbackend // ktlint-disable filename
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class BookappBackendApplication
+class BookappBackendApplication : SpringBootServletInitializer() {
 
-@RestController
-class IndexController {
-    @GetMapping("/")
-    fun index(): String {
-        return "Book app in development..."
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(BookappBackendApplication::class.java)
+    }
+
+    fun main(args: Array<String>) {
+        runApplication<BookappBackendApplication>(*args)
     }
 }
 
-fun main(args: Array<String>) {
-    runApplication<BookappBackendApplication>(*args)
+@RestController
+class NullResource {
+    @GetMapping
+    fun index() = "Book app in development..."
 }
