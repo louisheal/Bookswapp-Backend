9bd91d3b7857c81658f5e3bbb48c4895aef2fbfd
Emilian Filip <emilian.filip20@imperial.ac.uk>
2022-06-10 00:12:35 +0100

    Revert "[ef420] [fix] Attempt to fix Heroku deployment"
    This reverts commit 5bdf6c724252b6e560ebe9ad41f330492cfc51f8.

--- src/main/kotlin/ac/ic/drp19/bookappbackend/BookappBackendApplication.kt
+++ src/main/kotlin/ac/ic/drp19/bookappbackend/BookappBackendApplication.kt
@@ -1,26 +1,21 @@
-package ac.ic.drp19.bookappbackend // ktlint-disable filename
+package ac.ic.drp19.bookappbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class BookappBackendApplication : SpringBootServletInitializer() {
+class BookappBackendApplication
 
-    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
-        return application.sources(BookappBackendApplication::class.java)
-    }
-
-    fun main(args: Array<String>) {
-        runApplication<BookappBackendApplication>(*args)
+@RestController
+class IndexController {
+    @GetMapping("/")
+    fun index(): String {
+        return "Book app in development..."
     }
 }
 
-@RestController
-class NullResource {
-    @GetMapping
-    fun index() = "Book app in development..."
+fun main(args: Array<String>) {
+    runApplication<BookappBackendApplication>(*args)
 }
