d90295f8d173dae6968c0c9a05e408d41de13791
Emilian Filip <emilian.filip20@imperial.ac.uk>
2022-06-15 19:28:32 +0100

    [ef420] [fix] Fix syntax error in data.sql file.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ insert into Book (isbn, title, published) values ('1', 'Book1', '2000-1-1');
 insert into Book (isbn, title, published) values ('2', 'Book2', '2000-1-2');
 insert into Book (isbn, title, published) values ('3', 'Book3', '2000-1-3');
 
-insert into Users (username, passwdHash, name) values ('dev', 'dev', 'Dev Account')
+insert into Users (username, passwdHash, name) values ('dev', 'dev', 'Dev Account');
 insert into Users (username, passwdHash, name) values ('ef420_icl', '3451', 'Emil Filip');
 insert into Users (username, passwdHash, name) values ('Anand5329', '0368', 'Anand Doshi');
 insert into Users (username, passwdHash, name) values ('LJRex', '4796', 'Louis Heal');
