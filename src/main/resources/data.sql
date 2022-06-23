0306e024fc62903f193179bd001cd64551475d36
Aanjjey <av1620@ic.ac.uk>
2022-06-23 16:34:17 +0100

    [fix] db changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,6 @@
-insert into Book (isbn, title, published) values ('978-1529034523', 'The Hitchhiker''s Guide to ' ||
-                                                  'the Galaxy', '2020-3-5');
-insert into Book (isbn, title, published) values ('1', 'Book1', '2000-1-1');
-insert into Book (isbn, title, published) values ('2', 'Book2', '2000-1-2');
-insert into Book (isbn, title, published) values ('3', 'Book3', '2000-1-3');
+insert into Book (isbn, title, published) values ('978-1441529961', 'A Critique of Pure Physics:',
+                                                  '2020-3-5');
+insert into Book (isbn, title, published) values ('978-1292183398', 'Pure Mathematics', '2000-1-1');
 
 insert into Users (username, passwdHash, name) values ('dev', 'dev', 'Dev Account');
 insert into Users (username, passwdHash, name) values ('ef420_icl', '3451', 'Emil Filip');
@@ -12,23 +10,15 @@ insert into Users (username, passwdHash, name) values ('Anjey', '6699', 'Andrii 
 
 insert into Ownership (owner_id, book_id, totalCopies, currentCopies)
 values ((select id from Users where username='ef420_icl'),
-    (select id from Book where isbn='978-1529034523'),
-    1, 1);
-insert into Ownership (owner_id, book_id, totalCopies, currentCopies)
-values ((select id from Users where username='LJRex'),
-    (select id from Book where isbn='978-1529034523'),
+    (select id from Book where isbn='978-1441529961'),
     1, 1);
 insert into Ownership (owner_id, book_id, totalCopies, currentCopies)
 values ((select id from Users where username='dev'),
-    (select id from Book where isbn='1'),
+    (select id from Book where isbn='978-1292183398'),
     1, 1);
 
 insert into Ownership (owner_id, book_id, totalCopies, currentCopies)
 values ((select id from Users where username='Anand5329'),
-    (select id from Book where isbn='2'),
+    (select id from Book where isbn='978-1441529961'),
     1, 1);
 
-insert into Ownership (owner_id, book_id, totalCopies, currentCopies)
-values ((select id from Users where username='Anjey'),
-    (select id from Book where isbn='3'),
-    1, 1);
